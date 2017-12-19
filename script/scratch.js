d5f53e75db39c8e51b82a6de89324570fd692b36
Xayaseth Boudsady <boudsadyx@gmail.com>
2017-12-18 17:28:56 -0800

    First Commit

---
+++ script/scratch.js
@@ -0,0 +1,21 @@
+var students = [
+    { name: "Maddy" },
+    { name: "Adam" },
+    { name: "Jeseekia" },
+    { name: "Alexis" },
+    { name: "Kevin" },
+    { name: "Tim" },
+    { name: "Benson" },
+    { name: "Kwaku" },
+    { name: "Jennifer" },
+    { name: "Cara" },
+    { name: "Kaie" },
+    { name: "Michael" },
+    { name: "Shanita" },
+    { name: "Beth" }
+];
+
+
+students.forEach(function(students) {
+    console.log(students.name);
+});
